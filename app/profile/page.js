26c9b5a1959d01bdda70e536223c332314781d75
mahmudrkb <almahmudrkb98@gmail.com>
2025-01-24 16:03:22 +0600

    update profile

--- app/profile/page.js
+++ app/profile/page.js
@@ -1,7 +1,14 @@
-export default function Profile() {
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+
+export default async function Profile() {
+  const { isAuthenticated } = getKindeServerSession();
+  const isUserAuthenticated = await isAuthenticated();
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
   return (
-    <div>
-      <h1 className="text-red-700">Welcome to Profile</h1>
+    <div className=" flex justify-center items-center h-screen">
+      <h1 className="">Welcome to Profile</h1>
+      <h3 className="text-xl  ">This is me {user.given_name} </h3>
     </div>
   );
 }
